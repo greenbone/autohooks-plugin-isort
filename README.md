e44377d6612682387e178102c3edf8b8e43cd053
Michael Wiegand <michael.wiegand@greenbone.net>
2019-08-26 09:44:26 +0200

    Initialize repository
    This initializes the repository with a slightly modified version of
    [greenbone/autohooks-plugin-black](https://github.com/greenbone/autohooks-plugin-black).

---
+++ README.md
@@ -0,0 +1,68 @@
+![Greenbone Logo](https://www.greenbone.net/wp-content/uploads/gb_logo_resilience_horizontal.png)
+
+# autohooks-plugin-isort
+
+[![PyPI release](https://img.shields.io/pypi/v/autohooks-plugin-isort.svg)](https://pypi.org/project/autohooks-plugin-isort/)
+
+An [autohooks](https://github.com/greenbone/autohooks) plugin for Python code
+formatting via [isort](https://github.com/timothycrosley/isort).
+
+## Installation
+
+### Install using pip
+
+You can install the latest stable release of autohooks-plugin-isort from the
+Python Package Index using [pip](https://pip.pypa.io/):
+
+    pip install autohooks-plugin-isort
+
+Note the `pip` refers to the Python 3 package manager. In a environment where
+Python 2 is also available the correct command may be `pip3`.
+
+### Install using pipenv
+
+It is highly encouraged to use [pipenv](https://github.com/pypa/pipenv) for
+maintaining your project's dependencies. Normally autohooks-plugin-isort is
+installed as a development dependency.
+
+    pipenv install --dev autohooks-plugin-isort
+
+## Usage
+
+To activate the isort autohooks plugin please add the following setting to your
+*pyproject.toml* file.
+
+```toml
+[tool.autohooks]
+pre-commit = ["autohooks.plugins.isort"]
+```
+
+By default, autohooks plugin isort checks all files with a *.py* ending. If only
+the imports of files in a sub-directory or files with different endings should
+be sorted, just add the following setting:
+
+```toml
+[tool.autohooks]
+pre-commit = ["autohooks.plugins.isort"]
+
+[tool.autohooks.plugins.isort]
+include = ['foo/*.py', '*.foo']
+```
+
+## Maintainer
+
+This project is maintained by [Greenbone Networks GmbH](https://www.greenbone.net/).
+
+## Contributing
+
+Your contributions are highly appreciated. Please
+[create a pull request](https://github.com/greenbone/autohooks-plugin-isort/pulls)
+on GitHub. Bigger changes need to be discussed with the development team via the
+[issues section at GitHub](https://github.com/greenbone/autohooks-plugin-isort/issues)
+first.
+
+## License
+
+Copyright (C) 2019 [Greenbone Networks GmbH](https://www.greenbone.net/)
+
+Licensed under the [GNU General Public License v3.0 or later](LICENSE).
---
+++ autohooks/plugins/isort/isort.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2019 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+
+from autohooks.api import out, ok, error
+from autohooks.api.git import (
+    get_staged_status,
+    stage_files_from_status_list,
+    stash_unstaged_changes,
+)
+from autohooks.api.path import match
+
+DEFAULT_INCLUDE = ('*.py',)
+
+
+def check_isort_installed():
+    try:
+        import isort  # pylint: disable=unused-import
+    except ImportError:
+        raise Exception(
+            'Could not find isort. Please add isort to your python environment'
+        )
+
+
+def get_isort_config(config):
+    return config.get('tool', 'autohooks', 'plugins', 'isort')
+
+
+def get_include_from_config(config):
+    if not config:
+        return DEFAULT_INCLUDE
+
+    isort_config = get_isort_config(config)
+    include = isort_config.get_value('include', DEFAULT_INCLUDE)
+
+    if isinstance(include, str):
+        return [include]
+
+    return include
+
+
+def precommit(config=None, **kwargs):  # pylint: disable=unused-argument
+    out('Running isort pre-commit hook')
+
+    check_isort_installed()
+
+    include = get_include_from_config(config)
+    files = [f for f in get_staged_status() if match(f.path, include)]
+
+    if len(files) == 0:
+        ok('No staged files for isort available')
+        return 0
+
+    with stash_unstaged_changes(files):
+        for f in files:
+            try:
+                subprocess.check_call(['isort', '-q', str(f.absolute_path())])
+                ok('Running isort on {}'.format(str(f.path)))
+            except subprocess.CalledProcessError as e:
+                error('Running isort on {}'.format(str(f.path)))
+                raise e
+
+        stage_files_from_status_list(files)
+
+    return 0
