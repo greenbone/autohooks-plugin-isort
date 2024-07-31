6f43e69a47fd15b410619bca32a66712a9b1d134
Emmanuel Owosekun <emmanuel.owosekun@greenbone.net>
2024-07-31 13:34:42 +0200

    Add: keep imports in test_isort.py

--- tests/test_isort.py
+++ tests/test_isort.py
@@ -80,19 +80,6 @@ import sys
 import black
 
 import autohooks
-
-cmd = ["pylint", "autohooks/pluginrecommit_stages/pylint/pylint.py"]
-import subprocess  # pylint: disable=
-
-# status = subprocess.call(cmd)
-iofile = "tmp.txt"
-# status = subprocess.call(cmd, stdout=iofile)
-# blah blah lots of code ...
-
-status = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = status.communicate()
-print(out.decode(encoding="utf-8"))
-print(err.decode(encoding="utf-8"))
 """
         )
         with temp_git_repository() as temp_dir:
